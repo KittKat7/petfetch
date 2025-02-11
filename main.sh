222e073ede4d2dcf6117ddd1f3553dfb32235930
KittKat <122490347+KittKat7@users.noreply.github.com>
2025-02-11 12:22:10 -0500

    initial commit

---
+++ main.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+path="$(dirname "$0")"
+pet="cat"
+
+while test $# -gt 0; do
+	case "$1" in
+		-h|--help)
+			echo "help message"
+			exit 0
+			;;
+		--pet*)
+			shift
+			pet=`echo $1 | sed -e 's/^[^=]*=//g'`
+			shift
+			;;
+		*)
+			break
+			;;
+	esac
+done
+
+
+title="PetFetch - $pet"
+
+system=$(uname);
+arch=$(uname -m);
+version=$(uname -r);
+cpu=$(lscpu | grep 'Model name' | cut -f 2 -d ":" | awk '{$1=$1}1');
+
+info[0]=$title
+info+=("$system")
+info+=("$arch")
+info+=("$version")
+info+=("$cpu")
+
+pad="  "
+
+len=0;
+i=0;
+while IFS= read -r line; do
+	if [ ${#line} -ge $len ]
+	then
+		len=${#line}
+	fi
+	logo[$i]=$line
+	i=$i+1
+done < "$path/$pet.ascii"
+
+len=$(expr $len + ${#pad})
+
+j=0
+for i in "${logo[@]}"
+do
+	printf "%-${len}s%s\n" "$pad$i" "$pad: ${info[j]}"
+	j=$j+1
+done
+
+
+
+
+
+
